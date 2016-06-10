66a0f34517cdb6f3116526ac1ef882b4dfcbbdab
Eric Smith <esmith@8thlight.com>
2016-06-10 10:01:52 -0500

    Two Failing Tests!
    The second test fails because we don't actually have a server accepting
    clients, but the first test fails because the socket server doesn't
    always close! Awesome.
    
    No really it's a teaching exercise.

--- src/main/java/ServerSocketWrapper.java
+++ src/main/java/ServerSocketWrapper.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.function.Function;
 
 public class ServerSocketWrapper {
     private ServerSocket socket;
+    private Function<String, String> router;
 
     public void start(int port) throws IOException {
         socket = new ServerSocket(port);
@@ -10,5 +12,9 @@ public class ServerSocketWrapper {
 
     public void stop() throws IOException {
         socket.close();
+    }
+
+    public void setRouter(Function<String, String> router) {
+        this.router = router;
     }
 }
--- src/test/java/ServerSocketWrapperTest.java
+++ src/test/java/ServerSocketWrapperTest.java
@@ -1,8 +1,10 @@
 import org.junit.Test;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ConnectException;
 import java.net.Socket;
 import java.net.SocketException;
+import java.util.function.Function;
 
 import static junit.framework.TestCase.*;
 
@@ -13,8 +15,9 @@ public class ServerSocketWrapperTest {
         ServerSocketWrapper wrapper = new ServerSocketWrapper();
         wrapper.start(5000);
 
-        Socket socketThatWorks = new Socket("localhost", 5000);
-        assertTrue(socketThatWorks.isConnected());
+        try(Socket socketThatWorks = new Socket("localhost", 5000)) {
+            assertTrue(socketThatWorks.isConnected());
+        }
 
         wrapper.stop();
         try {
@@ -25,13 +28,26 @@ public class ServerSocketWrapperTest {
         }
     }
 
- /*   @Test
+    @Test
     public void itAcceptsDataOverTheConnection() throws IOException {
         ServerSocketWrapper wrapper = new ServerSocketWrapper();
         wrapper.start(5000);
 
         Socket socket = new Socket("localhost", 5000);
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+        out.println("data");
+        out.flush();
+
+        final String[] sentData = new String[1];
+        Function<String, String> router = string -> {
+            sentData[0] = string;
+            return "";
+        };
+
+        wrapper.setRouter(router);
+
+        assertEquals("data", sentData[0]);
 
         wrapper.stop();
-    }*/
+    }
 }
