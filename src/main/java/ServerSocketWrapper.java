db29168cb6d52e76362c2876aa1062907c63625b
Eric Smith <esmith@8thlight.com>
2016-06-15 08:41:06 -0700

    Start/Stop send data throws exception
    I tried a new approach which I like better after two tests. However,
    the third test now causes the first two two fail because you don't have
    the server properly stopping on every single test.

--- src/main/java/ServerSocketWrapper.java
+++ src/main/java/ServerSocketWrapper.java
@@ -1,5 +1,14 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ServerSocketWrapper {
-    public void start(int port) {
-        
+    private ServerSocket serverSocket;
+
+    public void start(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
+
+    public void stop() throws IOException {
+        serverSocket.close();
     }
 }
--- src/test/java/ServerSocketWrapperTest.java
+++ src/test/java/ServerSocketWrapperTest.java
@@ -1,11 +1,41 @@
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class ServerSocketWrapperTest {
 
     @Test
-    public void itAcceptsConnections() {
+    public void itStartsAndStopsAServer() throws IOException {
         ServerSocketWrapper wrapper = new ServerSocketWrapper();
         wrapper.start(5000);
 
+        Socket client = new Socket("localhost", 5000);
+        assertTrue(client.isConnected());
+    }
+
+    @Test(expected = ConnectException.class)
+    public void itStopsAServer() throws IOException {
+        ServerSocketWrapper wrapper = new ServerSocketWrapper();
+        wrapper.start(5000);
+        wrapper.stop();
+
+        new Socket("localhost", 5000);
+    }
+
+    @Test
+    public void itCanAcceptData() throws IOException {
+        ServerSocketWrapper wrapper = new ServerSocketWrapper();
+        wrapper.start(5000);
+
+        Socket client = new Socket("localhost", 5000);
+        PrintWriter out = new PrintWriter(client.getOutputStream());
+        out.println("sent data");
+        out.flush();
     }
 }
